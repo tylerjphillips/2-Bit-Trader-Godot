509a7b1df844e1b857096dfdaae737dcac3995d9
Tyler Phillips <tjphil06@gmail.com>
2020-01-19 17:50:00 -0600

    Added lines between overworld map locations
    Added map_neighbor_ids property to maps.json showing which locations each location is connected to. Note the id of each neighbor must appear in the other's list(eg map1 should have map2 and ma 2 should have map1) or unexpected behaviour will occur
    
    Added line generation to OverworldScreen which creates lines between locations
    
    Added OverworldRouteLine asset for actually drawing the lines

--- scripts/OverworldScreen.gd
+++ scripts/OverworldScreen.gd
@@ -6,6 +6,7 @@ signal update_game_data
 onready var root = get_tree().get_root().get_node("Root")
 
 onready var overworld_location_asset = preload("res://scenes/OverworldLocation.tscn")
+onready var overworld_route_line_asset = preload("res://scenes/OverworldRouteLine.tscn")
 
 onready var day_count_label = $OverworldInfoModule/OverworldInfoDayLabel
 onready var gold_count_label = $OverworldInfoModule/OverworldInfoGoldLabel
@@ -15,15 +16,31 @@ func _ready():
 
 func init(game_data):
 	# create overworld locations
+	var map_data = root.game_data["map_data"]
 	for map_id in root.game_data["map_data"]:
-		var map_data = root.game_data["map_data"][map_id]
-		var pos = map_data["map_location_coords"]
+		var current_map_data = root.game_data["map_data"][map_id]
+		var pos = current_map_data["map_location_coords"]
 		var overworld_location = self.overworld_location_asset.instance()
+		var pivot_offset = overworld_location.rect_pivot_offset		# used to make the locations centered
 		overworld_location.rect_position = Vector2(pos[0], pos[1])
 		self.add_child(overworld_location)
-		overworld_location.init(map_data)
+		overworld_location.init(current_map_data)
 		
 		overworld_location.connect("change_scene", self, "change_scene")
+		
+		# create lines between locations
+		for neighbor_id in current_map_data.get("map_neighbor_ids", []):
+			if neighbor_id < map_id:	# prevents duplicate lines
+				print("OverworldScreen: Creating line!!!!")
+				var neighbor_map_data = root.game_data["map_data"][neighbor_id]
+				var neighbor_pos = neighbor_map_data["map_location_coords"]
+				
+				var map_route_line = overworld_route_line_asset.instance()
+				self.add_child(map_route_line)
+				
+				map_route_line.add_point(Vector2(pos[0]+pivot_offset[0], pos[1]+pivot_offset[1]))
+				map_route_line.add_point(Vector2(neighbor_pos[0]+pivot_offset[0], neighbor_pos[1]+pivot_offset[1]))
+				print(map_route_line.points)
 		
 	# change text
 	day_count_label.text = str(root.game_data["main_data"]["day"])
