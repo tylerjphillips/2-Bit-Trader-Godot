6336521d46ea64dd06bca528deec61caab887129
Tyler Phillips <tjphil06@gmail.com>
2020-03-05 21:11:03 -0600

    XP bar interpolates color

--- scripts/UnitXPBar.gd
+++ scripts/UnitXPBar.gd
@@ -1,7 +1,11 @@
 extends ColorRect
 
 export var max_width = 16
+export var low_color = Color(.7,.7,1)
+export var high_color = Color(0,0,1)
 var unit
+
+onready var xp_tween = get_node("XPTween")
 
 onready var relay = get_node("/root/SignalRelay")
 
@@ -10,10 +14,16 @@ func _ready():
 
 func init(unit):
 	self.unit = unit
-	var width = float(unit.unit_xp) / max(1,unit.unit_xp_max) * self.max_width
-	self.set_size(Vector2(width, self.get_size()[1]))
+	interpolate_xp()
+	
+func interpolate_xp():
+	var new_width = float(unit.unit_xp) / max(1,unit.unit_xp_max) * float(self.max_width)
+	var new_size = Vector2(new_width, self.get_size()[1])
+	var new_color = lerp(low_color, high_color, float(unit.unit_xp) / max(1,unit.unit_xp_max))
+	self.set_size(new_size)
+	xp_tween.interpolate_property(self, "color", self.color, new_color, 1 ,Tween.TRANS_LINEAR, Tween.EASE_IN)
+	xp_tween.start()
 
 func _on_unit_xp_changed(unit):
 	if self.unit == unit:
-		var width = float(unit.unit_xp) / max(1,unit.unit_xp_max) * self.max_width
-		self.set_size(Vector2(width, self.get_size()[1]))
+		interpolate_xp()
