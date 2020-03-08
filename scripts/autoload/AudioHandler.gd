ff67e93cd0b487c42b2ab239c94679c4c4979f51
Tyler Phillips <tjphil06@gmail.com>
2020-03-08 18:33:43 -0500

    Added audio data file and level up sound
    Added a file which maps audio names to paths for misc audio files
    
    Added a sound effect on level up for units

--- scripts/autoload/AudioHandler.gd
+++ scripts/autoload/AudioHandler.gd
@@ -3,12 +3,14 @@ extends Node
 onready var audio_sample_asset = load("res://scenes/AudioAutomaticPlayer.tscn")
 
 onready var relay = get_node("/root/SignalRelay")
+onready var root = get_tree().get_root().get_node("Root")
 
 func _ready():
 	# listeners
 	relay.connect("unit_attacks_unit", self, "_on_unit_attacks_unit")
 	relay.connect("unit_attacks_tile", self, "_on_unit_attacks_tile")
 	relay.connect("unit_killed", self, "_on_unit_killed")
+	relay.connect("unit_leveled_up", self, "_on_unit_leveled_up")
 
 func _on_unit_attacks_tile(attacking_unit, tile_index, attacking_unit_attack_pattern, attacking_unit_weapon_data):
 	self.generate_audio_sample(attacking_unit_weapon_data["weapon_attack_audio_path"])
@@ -18,6 +20,9 @@ func _on_unit_attacks_unit(attacking_unit, weapon_data, attacked_unit, damage_ti
 
 func _on_unit_killed(killed_unit, killer_unit):
 	self.generate_audio_sample(killed_unit.unit_death_audio_path)
+	
+func _on_unit_leveled_up(unit):
+	generate_audio_sample(self.root.game_data["audio_data"]["unit_level_up_sound"])
 
 func generate_audio_sample(audio_path):
 	var audio_sample = audio_sample_asset.instance()
