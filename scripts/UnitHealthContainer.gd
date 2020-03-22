05f9f093f3fcfa4a9cb8980ab3103aad9e543e4d
Tyler Phillips <tjphil06@gmail.com>
2020-03-21 20:51:45 -0500

    Added support for damage preview on healthbars
    Added an animation for health modules to show how much damage will be done on an attack
    
    Added code to health bar container to allow for showing the animation. Currently always on and hardcoded to preview 2 damage

--- scripts/UnitHealthContainer.gd
+++ scripts/UnitHealthContainer.gd
@@ -9,12 +9,20 @@ func init(unit_health, unit_health_max):
 func _on_unit_health_changed(unit_health, unit_health_max):
 	generate_health_bar(unit_health, unit_health_max)
 	
-func generate_health_bar(unit_health, unit_max_health):
+func generate_health_bar(unit_health : int, unit_max_health : int, damage_preview = 2):
+	# free existing health bars
 	for i in self.get_children():
 		i.queue_free()
+	# generate a bar showing green icons for remaining health, and the rest are marked out
 	for index in range(unit_max_health):
 		var health = health_icon.instance();
 		self.add_child(health)
 		health.position = self.position + Vector2(8 * index, 0);
-		if index + 1 > unit_health: 
+		
+		# marked out health
+		if (index + 1 > unit_health): 
 			health.frame = 1
+		# damage preview animations
+		var remaining_health = unit_health - damage_preview
+		if ((index + 1 <= unit_health) and (index + 1 > remaining_health)): 
+			health.get_node("DamagePreviewPlayer").play("DamagePreview")
