3b3cedc3bb0c5e14f986e222782841f7e2f098aa
Tyler Phillips <tjphil06@gmail.com>
2020-03-28 22:43:05 -0500

    Added support for music for each screen
    AudioHandler can play music tracks on scene changes
    
    Added a track for shop music

--- scripts/autoload/AudioHandler.gd
+++ scripts/autoload/AudioHandler.gd
@@ -2,15 +2,30 @@ extends Node
 
 onready var audio_sample_asset = load("res://scenes/AudioAutomaticPlayer.tscn")
 
+var current_music_sample
+
 onready var relay = get_node("/root/SignalRelay")
 onready var root = get_tree().get_root().get_node("Root")
 
 func _ready():
 	# listeners
+	relay.connect("audio_finished", self, "_on_audio_finished")
 	relay.connect("unit_attacks_unit", self, "_on_unit_attacks_unit")
 	relay.connect("unit_attacks_tile", self, "_on_unit_attacks_tile")
 	relay.connect("unit_killed", self, "_on_unit_killed")
 	relay.connect("unit_leveled_up", self, "_on_unit_leveled_up")
+	relay.connect("change_scene", self, "_on_change_scene")
+
+func _on_change_scene(old_scene_name, new_scene_name):
+	# turn off the music on scene change
+	if current_music_sample != null:
+		self.current_music_sample.queue_free()
+		self.current_music_sample = null
+	if new_scene_name == "shop_screen":
+		self.current_music_sample = self.generate_audio_sample(self.root.game_data["audio_data"]["shop_music"])
+
+func _on_audio_finished(audio_path):
+	pass
 
 func _on_unit_attacks_tile(attacking_unit, tile_index, attacking_unit_attack_pattern, attacking_unit_weapon_data):
 	self.generate_audio_sample(attacking_unit_weapon_data["weapon_attack_audio_path"])
@@ -28,3 +43,4 @@ func generate_audio_sample(audio_path):
 	var audio_sample = audio_sample_asset.instance()
 	self.add_child(audio_sample)
 	audio_sample.init(audio_path)
+	return audio_sample
