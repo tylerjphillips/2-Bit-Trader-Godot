34894d7eb02f542b654d2a83ce3bc26068351ca9
Tyler Phillips <tjphil06@gmail.com>
2020-04-12 18:50:24 -0500

    Added remaining bonus xp label to party screen

--- scripts/AwardBonusXPButton.gd
+++ scripts/AwardBonusXPButton.gd
@@ -2,6 +2,8 @@ extends Button
 
 signal award_bonus_xp_button_up
 
+onready var remaining_bonus_xp_label = $RemainingBonusXPLabel
+
 onready var root = get_tree().get_root().get_node("Root")
 onready var relay = get_node("/root/SignalRelay")
 
@@ -9,7 +11,9 @@ func _ready():
 	self.connect("button_up", self, "_on_AwardBonusXPButton_up")
 	# emitters
 	self.connect("award_bonus_xp_button_up", relay, "_on_award_bonus_xp_button_up")
+	remaining_bonus_xp_label.text = str(self.root.game_data["main_data"]["bonus_xp"]) + " Remaining"
 	
 	
 func _on_AwardBonusXPButton_up():
 	emit_signal("award_bonus_xp_button_up")
+	remaining_bonus_xp_label.text = str(self.root.game_data["main_data"]["bonus_xp"]) + " Remaining"
